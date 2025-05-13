86a3512551093ef81910283dd0ee7891c1ac6c9c
Joao Geraldo <117011183+JoaoGeraldoS@users.noreply.github.com>
2025-05-13 09:20:26 -0300

    Delete src directory

--- src/main/java/com/talentos_banco/talentos_proz/alunos/repository/AlunoRepository.java
+++
@@ -1,7 +0,0 @@
-package com.talentos_banco.talentos_proz.alunos.repository;
-
-import com.talentos_banco.talentos_proz.alunos.model.AlunoModel;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface AlunoRepository extends JpaRepository<AlunoModel, Long> {
-}
--- src/main/java/com/talentos_banco/talentos_proz/cursos/repository/CursoRepository.java
+++
@@ -1,7 +0,0 @@
-package com.talentos_banco.talentos_proz.cursos.repository;
-
-import com.talentos_banco.talentos_proz.cursos.model.CursoModel;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface CursoRepository extends JpaRepository<CursoModel, Long> {
-}
