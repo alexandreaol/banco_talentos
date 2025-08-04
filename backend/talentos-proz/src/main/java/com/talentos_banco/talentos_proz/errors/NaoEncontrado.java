0415c60042bf9350e1e53677b6cb2ae8cf0397fa
Kauã Oliveira <kuab4289@gmial.com>
2025-08-04 17:28:47 -0300

    Removendo pasta do portfolio enviada por engano

--- backend/talentos-proz/src/main/java/com/talentos_banco/talentos_proz/errors/NaoEncontrado.java
+++
@@ -1,7 +0,0 @@
-package com.talentos_banco.talentos_proz.errors;
-
-public class NaoEncontrado extends RuntimeException {
-    public NaoEncontrado(String message) {
-        super(message);
-    }
-}
